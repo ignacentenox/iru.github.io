21a21f67ae87fc72c0033106551ce0131e7a66a8
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-01-01 20:16:52 -0300

    2024

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
-// Función para proponer matrimonio
 function proponerMatrimonio(respuesta) {
-    ocultarMensaje(); // Oculta el mensaje actual antes de mostrar la respuesta
+    ocultarMensaje();
     setTimeout(function () {
-        alert('¡Respuesta: ' + respuesta + '!'); // Muestra una alerta como ejemplo
-        mostrarMensaje(); // Muestra el mensaje nuevamente para futuras propuestas
-    }, 500); // Espera 500ms antes de mostrar la alerta
+        alert('¡Respuesta: ' + respuesta + '!');
+        mostrarMensaje();
+    }, 500);
 }
 
-// Función para mostrar un elemento
+function mostrarPropuesta() {
+    alert('¡Gracias por aceptar! ¡Te amo!');
+    ocultarCartel();
+}
+
+function rechazarPropuesta() {
+    alert('¡No hay problema! Seguimos siendo geniales juntos.');
+    ocultarCartel();
+}
+
 function mostrarElemento(elemento) {
     elemento.style.display = 'block';
 }
 
-// Función para ocultar un elemento
 function ocultarElemento(elemento) {
     elemento.style.display = 'none';
 }
 
-// Función para mostrar el mensaje
 function mostrarMensaje() {
     var mensajeElement = document.getElementById('message-section');
     mostrarElemento(mensajeElement);
 }
 
-// Función para ocultar el mensaje
 function ocultarMensaje() {
     var mensajeElement = document.getElementById('message-section');
     ocultarElemento(mensajeElement);
 }
 
-// Puedes agregar más funciones según tus necesidades
-
-// Función para mostrar el corazón
 function mostrarCorazon() {
     var corazonElement = document.getElementById('corazon');
     mostrarElemento(corazonElement);
 }
 
-// Puedes ajustar estas funciones según lo que desees hacer con la animación del corazón
+function mostrarCartel() {
+    var cartelFinal = document.getElementById('cartel-final');
+    mostrarElemento(cartelFinal);
+}
 
-// Llama a la función mostrarMensaje() cuando la página se carga
+function ocultarCartel() {
+    var cartelFinal = document.getElementById('cartel-final');
+    ocultarElemento(cartelFinal);
+}
+
+var video = document.getElementById('video-background');
+video.addEventListener('ended', function () {
+    mostrarCartel();
+});
+
 window.onload = function () {
     mostrarMensaje();
 };
